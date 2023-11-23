b1600145349394cd546c8086f6263385c4be90e9
Колега-розробник <orions3171@ukr.net>
2023-11-23 19:38:02 +0200

    Add project files.

---
+++ DZ_231123_Reviews(Repos and Sessions)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DZ_231123_Reviews_Repos_and_Sessions_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
